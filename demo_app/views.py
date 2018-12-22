a8c13de7dffed1de9dbf983b2f54a06822c13f06
Vagrant Default User <vagrant@contrib-stretch.localdomain>
2018-12-22 01:07:26 +0000

    css first and last name

--- demo_app/views.py
+++ demo_app/views.py
@@ -1,5 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import InputForm
+from .models import Customers
 
 # Create your views here.
 def index(request):
     return render(request, 'demo_app/index.html', {})
+
+def input_form(request):
+    if request.method == 'POST':
+        form = InputForm(request.POST)
+        if form.is_valid():
+            form.save()  # 保存
+            return redirect('result')
+    else:
+        form = InputForm()
+        return render(request, 'demo_app/input_form.html', {'form':form})
+
+def result(request):
+    return render(request, 'demo_app/result.html', {})
+    
